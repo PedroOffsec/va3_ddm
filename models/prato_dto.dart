76fc318c385a66984406226c4b7e4a478dc3cb48
jvmds <joaovitormelodossantos.1@gmail.com>
2023-06-19 08:42:35 -0300

    Remoção de arquivos não utilizados

--- models/prato_dto.dart
+++
@@ -1,8 +0,0 @@
-class PratoDto {
-  String urlImagem;
-  String nome;
-  String descricao;
-  double preco;
-
-  PratoDto(this.urlImagem, this.nome, this.descricao, this.preco);
-}
--- models/restaurante_dto.dart
+++
@@ -1,8 +0,0 @@
-import 'prato_dto.dart';
-
-class RestauranteDto {
-  String nome;
-  List<PratoDto> cardapio;
-
-  RestauranteDto(this.nome, this.cardapio);
-}
